c8409e58f3212a37a91434d25999a14880ae53b4
jimmypalelil <jimmypalelil@gmail.cm>
2018-12-30 18:54:32 -0700

    added update feature to lost and found

--- src/src/models/Inspection/createEmployees.py
+++
@@ -1,9 +0,0 @@
-from src.common.database import Database
-from src.models.Inspection.employee import Employee
-Database.go()
-
-def createEmployees():
-    names = ['Ruth K', 'Mary', 'Sharon', 'Bonny', 'Sunil', 'Pardeep', 'Harvinder']
-
-    for name in names:
-        Employee(name, 0, 0).insert()
