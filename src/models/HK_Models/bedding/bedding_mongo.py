f569aabc36621bd1fc911c02535910797a320af4
Jimmy Palelil <jimmypalelil@gmail.com>
2019-01-03 17:24:14 -0800

    added rooms for 2019 in housekeeping section

--- src/models/HK_Models/bedding/bedding_mongo.py
+++ src/models/HK_Models/bedding/bedding_mongo.py
@@ -1,12 +1,42 @@
 from src.common.database import Database
 from src.models.HK_Models.bedding.bedding import Bedding
+from src.models.HK_Models.carpet_shampoo.carpet_shampoo import Carpet
+from src.models.HK_Models.mattress.mattress import Mattress
+from src.models.HK_Models.pillow_protector.pillow_protector import Pillow
+from src.models.HK_Models.pillows.pillows import Pillows
+
 Database.go()
 
-roooms = Database.DATABASE['rooms'].find()
+rooms = Database.DATABASE['rooms'].find()
 
-months = ['jan', 'feb', 'mar', 'apr','may','jun', 'jul', 'aug', 'sep', 'oct', 'nov', 'dec']
+# # Beddings
+# months = ['jan', 'feb', 'mar', 'apr','may','jun', 'jul', 'aug', 'sep', 'oct', 'nov', 'dec']
+# for room in rooms:
+#     for month in months:
+#         Bedding(room['room_number'], room['type'], month, '2019', 'not done','beddings', room['_id']).insert()
+#
+# # Carpet Shampoo
+# months = ['jan to jun', 'jul to dec']
+# for room in rooms:
+#     for month in months:
+#         Carpet(room['room_number'], room['type'], month, '2019', 'not done', 'carpets', room['_id']).insert()
+#         Pillows(room['room_number'], room['type'], month, '2019', 'not done', 'pillowss', room['_id']).insert()
 
-
-for rom in roooms:
+# Bed Flips
+months = ['jan to mar', 'apr to jun', 'july to sep', 'oct to dec']
+for room in rooms:
     for month in months:
-        Bedding(rom['room_number'], rom['type'], month, '2018', 'not done','beddings', rom['_id']).insert()
+        Mattress(room['room_number'], room['type'], month, '2019', 'not done', 'mattress', room['_id']).insert()
+        Pillow(room['room_number'], room['type'], month, '2019', 'not done', 'pillows', room['_id']).insert()
+
+# # Pillow Protectors
+# months = ['jan to mar', 'apr to jun', 'july to sep', 'oct to dec']
+# for room in rooms:
+#     for month in months:
+#         Pillow(room['room_number'], room['type'], month, '2019', 'not done', 'pillows', room['_id']).insert()
+
+# # Pillows
+# months = ['jan to jun', 'jul to dec']
+# for room in rooms:
+#     for month in months:
+#         Pillows(room['room_number'], room['type'], month, '2019', 'not done', 'pillowss', room['_id']).insert()
