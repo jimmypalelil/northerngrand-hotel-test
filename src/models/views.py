e38f2f4979f4636aaea7df672cc204d3228c052f
Jimmy Palelil <jimmypalelil@gmail.com>
2019-01-10 12:59:38 -0800

    added selection feature in housekeeping section

--- src/models/views.py
+++ src/models/views.py
@@ -18,15 +18,15 @@ def returnList(type, year, month):
 @view_bp.route('/list/lost/<type>')
 def returnLostItems(type):
     if(type == 'lostItems'):
-      return dumps(Database.findAll('losts'))
+        return dumps(Database.findAll('losts'))
     return dumps(Database.findAll('returned'))
 
 @view_bp.route('/list/roomStatusChange', methods=['POST'])
 def changeStatus():
-    room = request.json
-    if(room['status'] == 'clean'):
-      room['status'] = 'not done'
-    else:
-      room['status'] = 'clean'
-    Database.update(room['cat'], {'_id': room['_id']}, room)
+    data = request.json
+    rooms = data[0]
+    status = data[1]
+    for room in rooms:
+        Database.DATABASE[room['cat']].update({'_id': room['_id']}, {"$set": {'status': status}})
+        # Database.update(room['cat'], {'_id': room['_id']}, room)
     return jsonify({'text': 'STATUS CHANGED SUCCESSFULLY'})
